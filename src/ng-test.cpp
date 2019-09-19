d4fed292d8b253738f6ef46ae59f320c64b9e996
marcus-sa <marcus-sa@outlook.com>
2019-09-19 23:46:26 +0200

    feat(): rewrote addon as a class

--- src/ng-test.cpp
+++ src/ng-test.cpp
@@ -1,50 +1,82 @@
-// https://myprogrammingnotes.com/simulate-mouse-click-using-qt-test-lib.html
+/* https://myprogrammingnotes.com/simulate-mouse-click-using-qt-test-lib.html */
 #include "ng-test.h"
-#include <QtTest/QtTest>
+#include "utils.h"
+#include <QTest>
 #include <QWidget>
 #include <qtest_widgets.h>
 #include <napi.h>
 
-void NgTest::KeyClick(const Napi::CallbackInfo& info) {
+Napi::FunctionReference NgTest::constructor;
+
+Napi::Object NgTest::Init(Napi::Env env, Napi::Object exports) {
+  Napi::Function func = DefineClass(env, "NgTest", {
+    InstanceMethod("keyClick", &NgTest::KeyClick),
+    InstanceMethod("keyPress", &NgTest::KeyPress),
+  });
+
+  constructor = Napi::Persistent(func);
+
+	constructor.SuppressDestruct();
+  exports.Set("NgTest", func);
+
+  return exports;
+}
+
+NgTest::NgTest(const Napi::CallbackInfo &info) : Napi::ObjectWrap<NgTest>(info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+
+  try {
+    Napi::Object nodeWidget = info[0].As<Napi::Object>();
+    /** @HINT
+     * We probably shouldn't do this as the NodeGUI API ought to change
+     * Instead let the end user provide the actual native widget themselves
+     */
+    Napi::Object native = nodeWidget.Get("native").As<Napi::Object>();
+
+    _widget = Napi::ObjectWrap<QWidget>::Unwrap(native);
+  } catch (...) {
+    Napi::TypeError::New(env, "Expected an instance of NodeWidget").ThrowAsJavaScriptException();
+  }
+}
+
+void NgTest::KeyPress(const Napi::CallbackInfo &info) {
+	Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+
+	int len = info.Length();
+
+	if (len < 1 || len > 3) {
+		return Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+	}
+
+	if (info[0].IsNumber()) {
+	    Qt::Key key = numToKey(info[0]);
+  	  QTest::keyClick(_widget, key);
+  	} else if (info[0].IsString()) {
+      char key = strToChar(info[0]);
+      QTest::keyClick(_widget, key);
+  	} else {
+  		return Napi::TypeError::New(env, "argument key must be a string").ThrowAsJavaScriptException();
+  	}
+}
+
+void NgTest::KeyClick(const Napi::CallbackInfo &info) {
 	Napi::Env env = info.Env();
 
-	Qt::KeyboardModifiers modifier;
-	int delay;
+	int len = info.Length();
 
-	if (info.Length() < 2) {
-		Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+	if (len < 1 || len > 3) {
+		return Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
 	}
 
-	Napi::Object nodeWidget = info[0].As<Napi::Object>();
-	QWidget *widget = Napi::ObjectWrap<QWidget>::Unwrap(nodeWidget);
-
-	if (!info[1].IsNumber()) {
-		Napi::TypeError::New(env, "key must be one of Key").ThrowAsJavaScriptException();
-	}
-
-	int nodeKey = info[1].As<Napi::Number>().Int32Value(); // static_cast<Qt::Key>(key);
-	Qt::Key key = static_cast<Qt::Key>(nodeKey);
-
-	if (info[2]) {
-		if (!info[2].IsNumber()) {
-			Napi::TypeError::New(env, "modifier must be one of KeyboardModifier").ThrowAsJavaScriptException();
-		}
-
-		int nodeModifier = info[2].As<Napi::Number>().Int32Value();
-		modifier = static_cast<Qt::KeyboardModifiers>(nodeModifier);
+	if (info[0].IsNumber()) {
+    Qt::Key key = numToKey(info[0]);
+	  QTest::keyClick(_widget, key);
+	} else if (info[0].IsString()) {
+    char key = strToChar(info[0]);
+    QTest::keyClick(_widget, key);
 	} else {
-		modifier = Qt::NoModifier;
+		return Napi::TypeError::New(env, "argument key must be a string").ThrowAsJavaScriptException();
 	}
-
-	if (info[3]) {
-		if (!info[3].IsNumber()) {
-			Napi::TypeError::New(env, "modifier must be one of KeyboardModifier").ThrowAsJavaScriptException();
-		}
-
-		delay = info[1].As<Napi::Number>().Int32Value();
-	} else {
-		delay = -1;
-	}
-
-	QTest::keyClick(widget, key, modifier, delay);
 }
