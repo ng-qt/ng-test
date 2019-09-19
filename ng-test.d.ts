e43d385b1b098c066f7acc4651060b43cfdb1b2d
marcus-sa <marcus-sa@outlook.com>
2019-09-19 23:52:59 +0200

    chore(): remove duplicate declaration

--- ng-test.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@ng-qt/qtest' {
-  import { Key, KeyboardModifier, NodeWidget } from '@nodegui/nodegui';
-
-  export function keyClick(widget: NodeWidget, key: Key, modifier?: KeyboardModifier, delay?: number): void;
-
-  export function keyClicks(widget: NodeWidget, text: string, modifier?: KeyboardModifier, delay?: number): void;
-}
