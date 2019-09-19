bf1c2a5800d7fbee496ccac9bd71e752df7b0c62
marcus-sa <marcus-sa@outlook.com>
2019-09-19 23:51:24 +0200

    feat(): add tests
    Reference: https://github.com/ng-qt/ng-test/issues/3

--- src/ng-test.spec.ts
+++ src/ng-test.spec.ts
@@ -1,15 +1,65 @@
-import { Key, QWidget } from '@nodegui/nodegui';
-// @ts-ignore
-import { keyClick } from '../index';
+import { Key, QWidget, QWidgetEvents } from '@nodegui/nodegui';
+import { NgTest } from '../index';
 
 describe('NgTest', () => {
-  describe('keyClick', () => {
-    it('should call', () => {
-      const node = new QWidget();
+  let ngTest: NgTest;
+  let widget: QWidget;
 
-      // node.native.subscribeToQtEvent(Key.Key_P);
+  beforeEach(() => {
+    widget = new QWidget();
+    ngTest = new NgTest(widget);
+  });
 
-      keyClick(node, Key.Key_P);
+  describe('constructor', () => {
+    it(`should throw an error if argument isn't an instance of NodeWidget`, () => {
+      // @ts-ignore
+      expect(() => new NgTest())
+        .toThrow('Expected an instance of NodeWidget');
+    });
+
+    it('should not throw an error when providing an instance of NodeWidget as argument', () => {
+      expect(() => new NgTest(new QWidget())).not.toThrow();
+    })
+  });
+
+  describe('keyPress', () => {
+    it('should call with character', (done) => {
+      const widget = new QWidget();
+      const ngTest = new NgTest(widget);
+
+      const spy = jest.fn();
+
+      // widget.addEventListener(QWidgetEvents.KeyPress, spy);
+
+      setImmediate(() => {
+        ngTest.keyPress('a');
+        expect(spy).not.toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('should call with key', () => {
+
     });
   });
+
+  /*describe('keyClick', () => {
+    let ngTest: NgTest;
+    let widget: QWidget;
+
+    beforeEach(() => {
+      widget = new QWidget();
+      ngTest = new NgTest(widget);
+    });
+
+    it('should call', (done) => {
+      widget.addEventListener(QWidgetEvents.MouseButtonDblClick, (event) => {
+        const keyEvent = new QKeyEvent(event);
+        console.log('YES');
+        done();
+      });
+
+      ngTest.keyClick(Key.Key_0);
+    });
+  });*/
 });
