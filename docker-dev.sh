7d0432df8f2d4d202df93b3ff5db9ead21db5603
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-09-20 09:28:59 +0200

    fix(docker): cd directly into work dir

--- docker-dev.sh
+++ docker-dev.sh
@@ -7,7 +7,7 @@ CONTEXT=$(pwd)
 # If container already exists, exec into it, otherwise run it
 if docker start $NAME &> /dev/null
 then
-  docker exec -it $NAME bash
+  docker exec -w $WORKDIR -it $NAME bash
 else
-  docker run --name $NAME -v "$CONTEXT":$WORKDIR -ti $IMAGE bash
+  docker run --name $NAME -v "$CONTEXT":$WORKDIR -w $WORKDIR -ti $IMAGE bash
 fi
