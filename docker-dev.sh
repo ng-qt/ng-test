c5392580557e2cd9ebccb5fe44022d4fce6551c3
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-09-20 14:03:37 +0200

    ci(): add default workflow

--- docker-dev.sh
+++ docker-dev.sh
@@ -1,14 +1,15 @@
 # For local development
-NAME=ng_test
-WORKDIR=/ng-test
-FILE=Dockerfile
-CONTEXT=$(pwd)
+NAME=${1:-ng-test}
+WORKDIR=/$NAME
+CMD=${2:-bash}
+FILE=${3:-dev.Dockerfile}
+CONTEXT=${4:-$(pwd)}
 
 # If container already exists, exec into it, otherwise run it
 if docker start $NAME &> /dev/null
 then
-  docker exec -w $WORKDIR -it $NAME bash
+  docker exec -w $WORKDIR -it $NAME $CMD
 else
 	docker build -f $FILE -t $NAME .
-  docker run --name $NAME -v "$CONTEXT":$WORKDIR -w $WORKDIR -it $NAME bash
+  docker run --name $NAME -v "$CONTEXT":$WORKDIR -w $WORKDIR -it $NAME $CMD
 fi
